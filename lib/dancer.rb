9b8d48fdf60ee0a05afd3d0ee53c9a0890065135
D1yr <alexander.m.sahli@gmail.com>
2020-08-12 14:02:43 -0400

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,7 +1,8 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative "../lib/fancy_dance.rb"
 
 class Dancer
-    extend FancyDance::ClassMethod 
-    include FancyDance::InstanceMethod 
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
 end
